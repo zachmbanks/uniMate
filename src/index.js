4ad9e3392f7700d8c0ca3245dae0ac35307bca8c
zachmbanks <zachmbanks@gmail.com>
2020-07-08 14:57:22 -0700

    Good save point before I jump into mathjs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = "Hello git connection test";
+// This creates an array of variables using the Array() constructor and the fill()
+let [roWidth, roHeight, outsideFrameWidth, outsideFrameHeight, shimGap] = Array(
+  5
+).fill(0);
+
+document.querySelector("body").innerHTML = `
+<label for="">Rough Opening Width</label>
+<br>
+<input id="roWidth" type="number" name="" value="${roWidth}">
+<br>
+<label for="">Rough Opening Width</label>
+<br>
+<input id="roHeight" type="number" name="" value="${roHeight}">
+<br>
+<button id="roSubmitBtn" type="submit">Submit Rough Opening</button>
+<br>
+<p>Based on the rough opening dimentions less 1/4" shims and caulkning 
+<br> 
+the outside door width is ${outsideFrameWidth}</p>
+`;
+document.querySelector("#roSubmitBtn").addEventListener("click", function() {
+  roWidth = document.querySelector("#roWidth").value;
+  roHeight = document.querySelector("#roHeight").value;
+  calculate();
+
+  // console.log(`The rough opening dimentions are ${roWidth}" x ${roHeight}"
+  // oustside frame dimention`);
+});
+
+function calculate() {
+  shimGap = 0.25;
+  outsideFrameWidth = roWidth - shimGap * 2;
+}
