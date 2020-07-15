2a6a0d238cf25b762364ffe7a687f3218619d364
zachmbanks <zachmbanks@gmail.com>
2020-07-15 15:35:26 -0700

    Adding Drawing functionality

--- src/index.js
+++ src/index.js
@@ -47,3 +47,16 @@ function nerestUsableFraction(decimalNumber, denominator) {
   return lastNumber;
   //
 }
+
+let c = document.getElementById("myCanvas");
+let ctx = c.getContext("2d");
+// ctx.moveTo(0, 0);
+// ctx.lineTo(300, 150);
+// ctx.stroke();
+
+// ctx.beginPath();
+// ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+// ctx.stroke()
+
+ctx.font = "60px Arial";
+ctx.strokeText("Hellow World", 20, 50);
